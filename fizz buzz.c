bd354d8e1386a3979ea5678fc05126ac5eb60194
Gus_5521 <163844692+GustavoAN0508@users.noreply.github.com>
2025-02-26 20:22:33 -0300

    Update fizz buzz.c

--- fizz buzz.c
+++ fizz buzz.c
@@ -4,10 +4,10 @@
 int main (void){
     for(int a = 0; a < 100; a++){
         if (a % 3 == 0)
-            printf("fizz");
+            printf("fizz\n");
         else if (a % 5 == 0)
-            printf("buzz");
+            printf("buzz\n");
         else
-            printf("%d", &a);
+            printf("%d\n", &a);
     }
 }
